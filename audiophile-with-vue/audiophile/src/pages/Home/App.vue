a346714fdad838cda30dee983928713002633997
Devika Sujith <devikasujith12@gmail.com>
2022-02-25 14:49:57 +0400

    Update structure

---
+++ audiophile-with-vue/audiophile/src/pages/Home/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from '../../components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
--- audiophile-with-vue/audiophile/vue.config.js
+++ audiophile-with-vue/audiophile/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pages: {
+    index: {
+      entry: './src/pages/Home/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Home | Audiophile',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+  },
 })
