ed9b34272c1bb10228f20c00f857d545d24b3dcd
Devika Sujith <devikasujith12@gmail.com>
2022-02-25 17:44:35 +0400

    Navbar mobile (WIP)

--- audiophile-with-vue/audiophile/src/components/Navbar.vue
+++ audiophile-with-vue/audiophile/src/components/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
   <nav>
     <div class="flex navbar-container">
-      <img src="@/assets/images/shared/desktop/logo.svg" alt="audiophile logo">
-      <ul class="flex">
+      <div class="flex">
+        <button @click="expandNav()" class="nav-toggle-mobile" data-visible="false"></button>
+        <img src="@/assets/images/shared/desktop/logo.svg" alt="audiophile logo">
+      </div>
+      <ul class="flex navbar" data-visible="false">
         <li><a href="#">Home</a></li>
         <li><a href="#">Headphones</a></li>
         <li><a href="#">Speakers</a></li>
@@ -17,6 +20,11 @@
 <script>
 export default {
   name: 'Navbar',
+  methods: {
+    expandNav() {
+      console.log('click');
+    }
+  }
 }
 </script>
 
